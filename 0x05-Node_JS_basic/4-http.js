6563531324a876cfdd65d7031a1e2eed3c79d019
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-10-21 18:57:11 +0000

    task 3

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,15 @@
 const http = require('http');
 
-// Create an HTTP server
+// Create the HTTP server
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-// Make the server listen on port 1245
+// Listen on port 1245
 app.listen(1245, () => {
-    console.log('Server is listening on port 1245');
+  console.log('Server is listening on port 1245');
 });
 
 // Export the app
